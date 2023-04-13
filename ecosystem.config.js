306e7685821ad05cf8419fd802152634ffca0620
DenDos <cotoha92@gmail.com>
2023-04-13 17:24:24 +0700

    tweak deploy config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,8 @@ module.exports = {
       autorestart: true, // Automatically restart the application if it crashes
       watch: false, // Watch for file changes and restart the application if necessary
       max_memory_restart: '450M', // Maximum memory allowed before restarting the application
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      max_size: '100M',
       env: {
         NODE_ENV: 'development', // Environment variables for the development environment
       },
